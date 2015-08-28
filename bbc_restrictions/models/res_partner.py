d443e66fe9e79d9cfcab446d535ee4d78991ef5a
Holger Brunn <hbrunn@therp.nl>
2015-08-28 16:19:55 +0200

    [ADD] fielsd_view_get override for locations

---
+++ bbc_restrictions/models/res_partner.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This module copyright (C) 2015 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from lxml import etree
+from openerp import models, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False,
+                        submenu=False):
+        result = super(ResPartner, self).fields_view_get(
+            view_id=view_id, view_type=view_type, toolbar=toolbar,
+            submenu=submenu)
+        if view_type != 'form':
+            return result
+        arch = etree.fromstring(result['arch'])
+        for field in arch.xpath("//field[@name='state_id']"):
+            field.attrib['options'] = \
+                "{'create': False, 'create_edit': False, 'no_open': True}"
+        result['arch'] = etree.tostring(arch)
+        return result
