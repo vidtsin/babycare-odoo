57d32aa936e06d3d4a902b66f4e19349009529e7
Stefan Rijnhart <stefan@opener.am>
2015-09-17 16:12:22 +0200

    [ADD] Recreate outgoing deliveries upon payment https://odoo.babycare.nl/web?db=babycare80#id=1311&view_type=form&model=project.task&action=541&active_id=14

---
+++ bbc_sale/README.rst
@@ -0,0 +1,4 @@
+Sale customizations for Babycare
+================================
+
+* When a payment is registered for a sales order that is in state 'Shipping exception', the outgoing delivery is recreated
---
+++ bbc_sale/models/account_invoice.py
@@ -0,0 +1,20 @@
+from openerp import models, api
+
+
+class Invoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def confirm_paid(self):
+        """
+        If an invoice gets paid of a sale order in shipment exception,
+        regenerate the picking. This covers the use case where the customer
+        pays after the shipping was cancelled manually after the due date
+        had already passed.
+        """
+        res = super(Invoice, self).confirm_paid()
+        self.env['sale.order'].search(
+            [('invoice_ids', 'in', self.ids),
+             ('state', '=', 'shipping_except')]
+        ).signal_workflow('ship_recreate')
+        return res
