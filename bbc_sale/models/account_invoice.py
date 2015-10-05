51f733f617416c6558ba707f7ef5fa7d5575d81b
Stefan Rijnhart <stefan@opener.am>
2015-10-05 14:42:15 +0200

    [ADD] Populate x_is_paid field on newly created pickings

--- bbc_sale/models/account_invoice.py
+++ bbc_sale/models/account_invoice.py
@@ -11,10 +11,18 @@ class Invoice(models.Model):
         regenerate the picking. This covers the use case where the customer
         pays after the shipping was cancelled manually after the due date
         had already passed.
+
+        Populate the manual field that registers if the picking's sale order
+        has been paid. For regular pickings, this happens in a custom server
+        action.
         """
         res = super(Invoice, self).confirm_paid()
-        self.env['sale.order'].search(
+        sale_orders = self.env['sale.order'].search(
             [('invoice_ids', 'in', self.ids),
-             ('state', '=', 'shipping_except')]
-        ).signal_workflow('ship_recreate')
+             ('state', '=', 'shipping_except')])
+        sale_orders.signal_workflow('ship_recreate')
+        sale_orders.refresh()
+        for sale_order in sale_orders:
+            if sale_order.invoiced:
+                sale_order.picking_ids.write({'x_is_paid': True})
         return res
