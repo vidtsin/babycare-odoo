8b7d77e974f0fd623a63a5e119f8008431228395
Stefan Rijnhart <stefan@opener.am>
2015-10-26 17:39:08 +0100

    [ADD] Clickable reference to SO or PO on picking. #1377

--- bbc_sale/models/stock_picking.py
+++ bbc_sale/models/stock_picking.py
@@ -5,6 +5,19 @@ class Picking(models.Model):
     _inherit = 'stock.picking'
 
     remarks = fields.Char(string='Remarks', compute='get_remarks')
+    purchase_id = fields.Many2one(
+        'purchase.order', string='Purchase order',
+        help='First of any related purchase orders in the system',
+        compute='_get_purchase_id')
+
+    @api.multi
+    def _get_purchase_id(self):
+        # Get the first of any related purchase orders
+        for picking in self:
+            for move in picking.move_lines:
+                if move.purchase_line_id:
+                    self.purchase_id = move.purchase_line_id.order_id
+                    continue
 
     @api.depends('move_lines')
     @api.one
