0f6367b2a60c9f9a97dd310a8748058b1a2d3df4
Stefan Rijnhart <stefan@opener.am>
2015-11-15 08:35:46 +0100

    [IMP] Performance of searching for purchases with missing stock

--- bbc_sale/models/purchase_order.py
+++ bbc_sale/models/purchase_order.py
@@ -8,7 +8,10 @@ class PurchaseOrder(models.Model):
 
     remarks = fields.Char()
     missing_stock = fields.Boolean(
-        compute='_get_missing_stock')
+        compute='_get_missing_stock',
+        search='_search_missing_stock',
+        help=('Indicates that virtual stock is below zero, but only for '
+              'unconfirmed purchases'))
     free_shipping = fields.Boolean(
         compute='_get_free_shipping',
         search='_search_free_shipping')
@@ -36,11 +39,32 @@ class PurchaseOrder(models.Model):
     @api.multi
     def _get_missing_stock(self):
         for order in self:
+            order.missing_stock = False
             if order.state not in ('draft', 'sent', 'bid'):
                 # Ignored anyway
                 continue
-            order.missing_stock = False
             for line in order.order_line:
                 if line.product_id.virtual_available < 0:
                     order.missing_stock = True
                     break
+
+    @api.multi
+    def _search_missing_stock(self, operator, value):
+        if operator not in ('=', '==') and not isinstance(value, bool):
+            raise UserError(
+                'This query on the "missing stock" field is not supported. '
+                'You can only search on "= True" or "= False"')
+        self.env.cr.execute(
+            """
+            SELECT pol.product_id, pol.order_id FROM purchase_order_line pol,
+                purchase_order po
+            WHERE pol.order_id = po.id
+            AND po.state in ('draft', 'sent', 'bid');
+            """)
+        product_order = self.env.cr.fetchall()
+        products = self.env['product.product'].browse(
+            [x[0] for x in product_order]).filtered(
+            lambda p: p.virtual_available < 0)
+        purchase_ids = [x[1] for x in product_order
+                        if (x[0] in products.ids) == value]
+        return [('id', 'in', purchase_ids)]
