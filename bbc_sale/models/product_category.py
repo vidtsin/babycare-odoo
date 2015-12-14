1482e8252599f9d726097201aece782fd8575d08
Stefan Rijnhart <stefan@opener.am>
2015-12-14 10:16:44 +0100

    [DEL] Remaining attribute

--- bbc_sale/models/product_category.py
+++ bbc_sale/models/product_category.py
@@ -4,7 +4,6 @@ from openerp import models, fields
 
 class Category(models.Model):
     _inherit = 'product.category'
-    _order = 'parent_sequence, sequence'
 
     # Set a default for the existing sequence field
     sequence = fields.Integer(default=1)
