aba17fe98a42c10631f486eeaca21b3da9eb3895
Stefan Rijnhart <stefan@opener.am>
2016-10-02 20:37:22 +0200

    [ADD] Restrict project selection fields (#2307)

---
+++ bbc_project/README.rst
@@ -0,0 +1,5 @@
+Project customizations for Babycare
+===================================
+
+* Restrict users to assign project tasks to to members of the Employee group
+* Restrict partners to link project tasks to to project customers
---
+++ bbc_project/__openerp__.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+# Copyright (C) 2016 Opener B.V. <https://opener.am>
+# @author Stefan Rijnhart <stefan@opener.am>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Babycare Project customizations",
+    "category": "Sale",
+    "version": "8.0.1.0",
+    "author": "Opener B.V.",
+    "website": 'https://opener.am',
+    "depends": [
+        'project',
+    ],
+    'data': [
+        'views/project_task.xml',
+    ],
+}
