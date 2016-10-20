5cd4353bdb918202a965955b90e02fe32c8b54b6
Stefan Rijnhart <stefan@opener.am>
2016-10-20 20:47:52 +0200

    [ADD] POS Order count #2310

--- bbc_sale/models/res_partner.py
+++ bbc_sale/models/res_partner.py
@@ -9,6 +9,15 @@ class Partner(models.Model):
         'Free shipping from',
         help=('The amount from which purchase orders are shipped out free '
               'from this supplier'))
+    pos_order_count = fields.Integer(
+        compute='_pos_order_count', string='# of POS Orders')
+
+    @api.multi
+    def _pos_order_count(self):
+        for partner in self:
+            partner.pos_order_count = len(
+                self.env['pos.order'].search(
+                    [('partner_id', '=', partner.id)]))
 
     @api.model
     def _commercial_fields(self):
