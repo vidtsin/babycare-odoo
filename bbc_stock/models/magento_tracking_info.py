3141f6e000ca481f15f8fbb434f803a3aa892669
Stefan Rijnhart <stefan@opener.am>
2017-02-07 08:52:37 +0100

    [ADD] Sync number of packages on the picking

---
+++ bbc_stock/models/magento_tracking_info.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from openerp import api, models
+
+
+class TrackingInfo(models.Model):
+    _inherit = 'magento.tracking.info'
+
+    @api.multi
+    def unlink(self):
+        pickings = self.mapped('wk_picking_id')
+        res = super(TrackingInfo, self).unlink()
+        for picking in pickings:
+            picking.number_of_packages = len(
+                picking.magento_carrier_ids)
+        return res
+
+    @api.model
+    def create(self, vals):
+        res = super(TrackingInfo, self).create(vals)
+        if res.wk_picking_id:
+            res.wk_picking_id.number_of_packages = len(
+                res.wk_picking_id.magento_carrier_ids)
+        return res
