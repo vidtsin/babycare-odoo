29058393e3f24b263f604f6cbe151bc106a0d7da
Stefan Rijnhart <stefan@opener.am>
2017-03-20 07:52:58 +0100

    [ADD] #2430, search attachments for indexed content

---
+++ bbc_document/README.rst
@@ -0,0 +1,5 @@
+Babycare Document customizations
+================================
+
+* Add a menu item for attachments under Knowledge main menu
+* Search indexed content by default
---
+++ bbc_document/__openerp__.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+# Copyright (C) 2016 Opener B.V. <https://opener.am>
+# @author Stefan Rijnhart <stefan@opener.am>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Babycare Document customizations",
+    "category": "Document Management System",
+    "version": "8.0.1.0",
+    "author": "Opener B.V.",
+    "website": 'https://opener.am',
+    "depends": [
+        'document',
+    ],
+    'data': [
+        'views/ir_attachment.xml',
+    ],
+}
