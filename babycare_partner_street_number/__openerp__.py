61798d77f96c811afe2bafd6344695e90d0f202d
babycarenl <10392890+wytzejan@users.noreply.github.com>
2017-09-18 13:02:55 +0200

    [babycare_partner_street_number][ADD] new extension module for street number addition

---
+++ babycare_partner_street_number/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Babycare Extension: Street name and number",
+    "summary": "Introduces new field for street number addition.",
+    "version": "8.0.0.1.0",
+    "author": "Wytze Jan Riedstra,Therp BV,Odoo Community Association (OCA)",
+    "category": 'Tools',
+    "depends": [
+        'partner_street_number',
+    ],
+    "data": [
+        'views/res_partner.xml',
+    ],
+    'license': 'AGPL-3',
+}
