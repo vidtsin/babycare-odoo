fdcf3bbde37c18f452cd7b9b57e6052e802e4c45
babycarenl <10392890+wytzejan@users.noreply.github.com>
2018-01-08 11:22:34 +0100

    [REM] pre migration removed because it is unnecessary when only adding translations.

--- bbc_stock/__openerp__.py
+++ bbc_stock/__openerp__.py
@@ -5,7 +5,7 @@
 {
     "name": "Babycare Stock customizations",
     "category": "Stock",
-    "version": "1.1",
+    "version": "1.0",
     "author": "Opener B.V.",
     "website": 'https://opener.am',
     "depends": [
--- bbc_stock/migrations/8.0.1.1/pre-migrate.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-def migrate(cr, version):
-    if not version:
-        return
-    cr.execute(
-        """ DELETE FROM ir_translation
-            WHERE module = 'bbc_stock'; """)
